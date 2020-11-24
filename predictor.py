b7482ed618d51257371d5f972f17fb22fc7980a2
b <brightmart@hotmail.com>
2020-11-25 02:48:16 +0800

    update

--- predictor.py
+++ predictor.py
@@ -16,7 +16,7 @@ import uuid
 import time
 import urllib
 # -- own --
-
+from projector_factor import projector_factor_fn
 
 # -- code --
 def get_url_image_and_return_path(url_image):
@@ -24,7 +24,7 @@ def get_url_image_and_return_path(url_image):
     #image = np.asarray(bytearray(resp.read()), dtype="uint8")
     #image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    file_path = "./imgs/" + str(uuid.uuid1())+".jpg" # "test.jpg"
+    file_path = "./images/" + str(uuid.uuid1())+".jpg" # "test.jpg"
 
     urllib.request.urlretrieve(url_image, file_path)
     # img = request.files['image']
@@ -76,35 +76,20 @@ class PythonPredictor:
         # 获取图片内容
         image_path = get_url_image_and_return_path(imageurl) # o.k.
         # 得到latent code
-        step1_string_inverse='python3 projector_factor.py --ckpt "./models/pretrain-model-FFHQ-256-550000.pt" --fact "./models/GAN_space_factor_550k.pt "'+image_path
-        os.system(step1_string_inverse)
+        ckpt="./models/pretrain-model-FFHQ-256-550000.pt"
+        fact="./models/GAN_space_factor_550k.pt"
+        save_latent_image_path, save_latent_code_path=projector_factor_fn(ckpt, fact, [image_path])
+        print("predict.save_latent_image_path:",save_latent_image_path)
+        print("predict.save_latent_code_path:",save_latent_code_path)
+
         step1_time_end=time.time()
         print("predict.time spent:",str(step1_time_end-step1_time_start),"step 1 completed.")
-        # 推理
-        #results = self.model(image.to(self.device))
+
+        # Step 4: LS Image generation with multiple styles. We use the in versed code to generate images with multiple style in the target domain
+        # !python3 gen_multi_style.py --model1 "/content/drive/My Drive/Colab Notebooks/I2I_StyleGAN2-2/deploy/pretrain-model-FFHQ-256-550000.pt" --model2 "/content/drive/My Drive/Colab Notebooks/I2I_StyleGAN2-2/deploy/fine_tuned_model_003000.pt" --fact "/content/drive/My Drive/Colab Notebooks/I2I_StyleGAN2-2/b5WechatIMG57.pt" --fact_base "/content/drive/My Drive/Colab Notebooks/I2I_StyleGAN2-2/deploy/GAN_space_factor_550k.pt" -o "/content/drive/My Drive/Colab Notebooks/I2I_StyleGAN2-2/output_b576/" --swap_layer 3 --stylenum 10
         print("predict.ended...")
 
 
-    def predict_example(self, json):
-        # 从 json.url 获取图片的 url
-        imageurl = json["url"] # o.k.
-        # 获取图片内容
-        image = get_url_image_and_return_path(imageurl) # o.k.
-        # 图片预处理
-        image = self.transform(image) # o.k.
-        image = torch.tensor(image.numpy()[np.newaxis, ...])
-
-        # 推理
-        results = self.model(image.to(self.device))
-
-        # 获取结果前五
-        top5_idx = results[0].sort()[1][-5:]
-
-        # 获取前五分类的名称
-        top5_labels = [self.idx2label[idx] for idx in top5_idx]
-        top5_labels = top5_labels[::-1]
-
-        return top5_labels
 
 
 if __name__ == '__main__':
--- projector_factor.py
+++ projector_factor.py
@@ -13,6 +13,7 @@ from utils import AddPepperNoise
 import lpips
 from model import Generator
 
+factor_base_path='./models/'
 #torch.manual_seed(111)
 
 def noise_regularize(noises):
@@ -73,6 +74,168 @@ def make_image(tensor):
         .numpy()
     )
 
+def projector_factor_fn(ckpt,fact,files):
+    device = "cuda"
+
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("--ckpt", type=str, required=True)
+    parser.add_argument("--size", type=int, default=256)
+    parser.add_argument("--lr_rampup", type=float, default=0.05)
+    parser.add_argument("--lr_rampdown", type=float, default=0.25)
+    parser.add_argument("--lr", type=float, default=0.1)
+    parser.add_argument("--noise", type=float, default=0.05)
+    parser.add_argument("--noise_ramp", type=float, default=0.75)
+    parser.add_argument("--step", type=int, default=1000)
+    parser.add_argument("--noise_regularize", type=float, default=1e5)
+    parser.add_argument("--mse", type=float, default=0)
+    parser.add_argument("--w_plus", action="store_true")
+    parser.add_argument("--device", type=str, default="cuda")
+    # parser.add_argument("--fact", type=str, required=True)
+    # parser.add_argument("files", metavar="FILES", nargs="+")
+
+    args = parser.parse_args()
+    eigvec = torch.load(fact)["eigvec"].to(args.device) # args.fact
+    eigvec.requires_grad = False
+
+    n_mean_degree = 10000
+    n_mean_weight = 10000
+
+    resize = min(args.size, 256)
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize(resize),
+            transforms.CenterCrop(resize),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        ]
+    )
+
+    imgs = []
+
+    for imgfile in files: # args.files
+         img = transform(Image.open(imgfile).convert("RGB"))
+         imgs.append(img)
+
+    imgs = torch.stack(imgs, 0).to(device)
+
+    g_ema = Generator(args.size, 512, 8)
+    g_ema.load_state_dict(torch.load(ckpt)["g_ema"], strict=False) # args.ckpt
+    g_ema.eval()
+    g_ema = g_ema.to(device)
+    trunc = g_ema.mean_latent(4096)
+
+    with torch.no_grad():
+        noise_sample = torch.randn(1, 512, device=device)
+        latent = g_ema.get_latent(noise_sample)
+
+        degree_sample = torch.randn(n_mean_degree, 1, device=device) * 1000
+        weight_sample = torch.randn(n_mean_weight, 512, device=device).unsqueeze(1)  # row of factor
+
+        degree_mean = degree_sample.mean(0)
+        degree_std = ((degree_sample - degree_mean).pow(2).sum() / n_mean_degree) ** 0.5
+
+        weight_mean = weight_sample.mean(0)
+        weight_std = ((weight_sample - weight_mean).pow(2).sum() / n_mean_weight) ** 0.5
+
+        direction = torch.mm(eigvec, weight_mean.T)
+
+    percept = lpips.PerceptualLoss(
+        model="net-lin", net="vgg", use_gpu=device.startswith("cuda")
+    )
+
+    noises_single = g_ema.make_noise()
+    noises = []
+    for noise in noises_single:
+        noises.append(noise.repeat(imgs.shape[0], 1, 1, 1).normal_())
+
+    direction_in = direction.T
+    weight_in = weight_mean
+
+    weight_mean.requires_grad = True
+
+    for noise in noises:
+        noise.requires_grad = True
+
+    optimizer = optim.Adam([weight_mean] + noises, lr=args.lr)
+
+    pbar = tqdm(range(args.step))
+    latent_path = []
+    weight_path = []
+
+    imgs.detach()
+
+    for i in pbar:
+        t = i / args.step
+        lr = get_lr(t, args.lr)
+        optimizer.param_groups[0]["lr"] = lr
+        noise_strength = weight_std * args.noise * max(0, 1 - t / args.noise_ramp) ** 2
+        weight_n = latent_noise(weight_in, noise_strength.item())
+        direction_n = torch.mm(weight_in, eigvec)
+
+        img_gen, _ = g_ema([direction_n], input_is_latent=True, noise=noises)
+
+        batch, channel, height, width = img_gen.shape
+
+        if height > 256:
+            factor = height // 256
+
+            img_gen = img_gen.reshape(
+                batch, channel, height // factor, factor, width // factor, factor
+            )
+            img_gen = img_gen.mean([3, 5])
+
+        p_loss = percept(img_gen, imgs).sum()
+        n_loss = noise_regularize(noises)
+        mse_loss = F.mse_loss(img_gen, imgs)
+
+        loss = p_loss + args.noise_regularize * n_loss + args.mse * mse_loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        noise_normalize_(noises)
+
+        if (i + 1) % 100 == 0:
+            direction_in = torch.mm(weight_in, eigvec)
+            latent_path.append(direction_in.detach().clone())
+            weight_path.append(weight_in.detach().clone())
+
+        pbar.set_description(
+            (
+                f"perceptual: {p_loss.item():.4f}; noise regularize: {n_loss.item():.4f};"
+                f" mse: {mse_loss.item():.4f}; lr: {lr:.4f}"
+            )
+        )
+
+    img_gen, _ = g_ema([latent_path[-1]], input_is_latent=True, noise=noises)
+
+    filename = os.path.splitext(os.path.basename(files[0]))[0] + ".pt" # args.files[0]
+
+    img_ar = make_image(img_gen)
+
+    result_file = {}
+    for i, input_name in enumerate(files): # args.files
+        noise_single = []
+        for noise in noises:
+            noise_single.append(noise[i: i + 1])
+
+        result_file[input_name] = {
+            "img": img_gen[i],
+            "latent": latent_path[i],
+            "weight": weight_path[i],
+            "noise": noise_single,
+        }
+
+        img_name = os.path.splitext(os.path.basename(input_name))[0] + "-project.png"
+        pil_img = Image.fromarray(img_ar[i])
+        save_latent_image_path= factor_base_path + img_name
+        pil_img.save(save_latent_image_path)  # add factor_base_path
+
+    save_latent_code_path=factor_base_path + filename
+    torch.save(result_file,save_latent_code_path)  # factor_base_path
+    return save_latent_image_path, save_latent_code_path
 
 if __name__ == "__main__":
     device = "cuda"
@@ -231,6 +394,6 @@ if __name__ == "__main__":
 
         img_name = os.path.splitext(os.path.basename(input_name))[0] + "-project.png"
         pil_img = Image.fromarray(img_ar[i])
-        pil_img.save(img_name)
+        pil_img.save(factor_base_path+img_name) # add factor_base_path
 
-    torch.save(result_file, filename)
+    torch.save(result_file, factor_base_path+filename) # factor_base_path
